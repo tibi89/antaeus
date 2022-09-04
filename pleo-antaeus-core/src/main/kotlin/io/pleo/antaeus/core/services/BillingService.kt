6dd814c5de43b30d909024e759f23a5209b8ba26
Tiberiu Ionescu <tiberiu.ionescu@wolt.com>
2022-09-04 17:20:29 +0200

    Initial logic for billing service for a single payment provider

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,41 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.InvoiceStatus
+import mu.KotlinLogging
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val paymentProvider: PaymentProvider,
+    private val currency: Currency,
+    private val invoiceService: InvoiceService
 ) {
-// TODO - Add code e.g. here
+    private val logger = KotlinLogging.logger {}
+
+    fun runningBillingForProvider() {
+        invoiceService.fetchInvoicesByCurrency(currency).forEach {
+            if (it.status == InvoiceStatus.PENDING) {
+                try {
+                    val result = paymentProvider.charge(it)
+                    if (result) {
+                        invoiceService.markInvoiceAsPaid(it.id)
+                    } else {
+                        invoiceService.markInvoiceAsFailed(it.id)
+                    }
+                } catch (e: CurrencyMismatchException) {
+                    logger.error { "Invoice doesn't have the same currency as the one expected by the payment provider" }
+                    invoiceService.markInvoiceAsFailed(it.id)
+                } catch (e: CustomerNotFoundException) {
+                    logger.error { "Customer not found for invoice ${it.id}" }
+                    invoiceService.markInvoiceAsFailed(it.id)
+                } catch (e: NetworkException) {
+                    logger.error { "Network error when trying to charge invoice ${it.id}" }
+                    invoiceService.markInvoiceAsRetryable(it.id)
+                }
+            }
+        }
+    }
 }
--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/InvoiceService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/InvoiceService.kt
@@ -6,7 +6,9 @@ package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
+import io.pleo.antaeus.models.Currency
 import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 
 class InvoiceService(private val dal: AntaeusDal) {
     fun fetchAll(): List<Invoice> {
@@ -16,4 +18,17 @@ class InvoiceService(private val dal: AntaeusDal) {
     fun fetch(id: Int): Invoice {
         return dal.fetchInvoice(id) ?: throw InvoiceNotFoundException(id)
     }
+
+    fun fetchInvoicesByCurrency(currency: Currency): List<Invoice> {
+        return dal.fetchInvoicesByCurrency(currency)
+    }
+
+    fun markInvoiceAsPaid(id: Int) =
+        dal.updateStatusOfInvoice(id, InvoiceStatus.PAID)
+
+    fun markInvoiceAsFailed(id: Int) =
+        dal.updateStatusOfInvoice(id, InvoiceStatus.FAILED)
+
+    fun markInvoiceAsRetryable(id: Int) =
+        dal.updateStatusOfInvoice(id, InvoiceStatus.PENDING)
 }
