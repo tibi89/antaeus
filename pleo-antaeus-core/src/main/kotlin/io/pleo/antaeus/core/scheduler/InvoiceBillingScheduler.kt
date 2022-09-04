914fe10a383424ae943e095d2eb924f6999ece1d
Ionescu Tiberiu <ionescutiberiu22@gmail.com>
2022-09-04 20:58:38 +0200

    Add test for the invoice billing scheduler

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/scheduler/InvoiceBillingScheduler.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/scheduler/InvoiceBillingScheduler.kt
@@ -3,12 +3,14 @@ package io.pleo.antaeus.core.scheduler
 import io.pleo.antaeus.core.services.BillingService
 import io.pleo.antaeus.models.Currency
 import mu.KotlinLogging
+import java.time.Clock
 import java.time.LocalDate
 import kotlin.concurrent.timer
 import kotlin.random.Random
 
 class InvoiceBillingScheduler(
-    private val billingServicesToCurrency: Map<Currency, BillingService> // fixme: this should be for country, since we can have a different billing service for currency and country
+    private val billingServicesToCurrency: Map<Currency, BillingService>, // fixme: this should be for country, since we can have a different billing service for currency and country
+    private val clock: Clock = Clock.systemDefaultZone()
 ) {
     private val logger = KotlinLogging.logger {}
 
@@ -23,25 +25,30 @@ class InvoiceBillingScheduler(
     private fun scheduleRecurringTask(currency: Currency, billingService: BillingService) {
         // starting the job randomly between 0 and 10 seconds so that not all jobs are started at the same time
         timer(name = "BillingService-${currency.name}", daemon = true, period = 10000, initialDelay = Random.nextLong(10000)) {
-            // we need to run this job only on the first and second day of the month (second day since we can retry invoices),
-            // doing it like this since we want to run it multiple times
-            // per day, but only on the first and second day of the month, which is not possible with the scheduler
-            if (!shouldRun()) {
-                logger.info { "Not running today for currency ${currency.name}" }
-                return@timer
-            }
-            try {
-                billingService.runningBillingForProvider()
-            } catch (e: Exception) {
-                logger.error(e) { "Error while running billing for ${currency.name}" }
-            }
+            task(currency, billingService)
+        }
+    }
+
+    // this is only public for testing purposes
+    fun task(currency: Currency, billingService: BillingService) {
+        // we need to run this job only on the first and second day of the month (second day since we can retry invoices),
+        // doing it like this since we want to run it multiple times
+        // per day, but only on the first and second day of the month, which is not possible with the scheduler
+        if (!shouldRun()) {
+            logger.info { "Not running today for currency ${currency.name}" }
+            return
+        }
+        try {
+            billingService.runningBillingForProvider()
+        } catch (e: Exception) {
+            logger.error(e) { "Error while running billing for ${currency.name}" }
         }
     }
 
     private fun shouldRun(): Boolean {
         // fixme: If the billing is done in multiple countries with multiple timezones,
         // then this might have some small issues, as it takes the timezone of the server
-        val dayOfMonth = LocalDate.now().dayOfMonth
+        val dayOfMonth = LocalDate.now(clock).dayOfMonth
         return dayOfMonth == 1 || dayOfMonth == 2
     }
 }
---
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/anteus/core/scheduler/InvoiceBillingSchedulerTest.kt
@@ -0,0 +1,43 @@
+package io.pleo.anteus.core.scheduler
+
+import io.mockk.mockk
+import io.mockk.verify
+import io.pleo.antaeus.core.scheduler.InvoiceBillingScheduler
+import io.pleo.antaeus.core.services.BillingService
+import io.pleo.antaeus.models.Currency
+import org.junit.jupiter.api.Test
+import java.time.Clock
+import java.time.Instant
+import java.time.ZoneId
+
+class InvoiceBillingSchedulerTest {
+    private val billingService = mockk<BillingService>(relaxed = true)
+
+    @Test
+    fun `will not run task`() {
+        val scheduler = InvoiceBillingScheduler(mapOf(Currency.EUR to billingService), Clock.systemUTC())
+        scheduler.task(Currency.EUR, billingService)
+        verify(exactly = 0, timeout = 11_000) { billingService.runningBillingForProvider() }
+    }
+
+    @Test
+    fun `will run task on the first day of the month`() {
+        val scheduler = InvoiceBillingScheduler(mapOf(Currency.EUR to billingService), Clock.fixed(Instant.parse("2020-01-01T00:00:00Z"), ZoneId.of("UTC")))
+        scheduler.task(Currency.EUR, billingService)
+        verify(exactly = 1, timeout = 11_000) { billingService.runningBillingForProvider() }
+    }
+
+    @Test
+    fun `will run task on the second day of the month`() {
+        val scheduler = InvoiceBillingScheduler(mapOf(Currency.EUR to billingService), Clock.fixed(Instant.parse("2020-01-02T00:00:00Z"), ZoneId.of("UTC")))
+        scheduler.task(Currency.EUR, billingService)
+        verify(exactly = 1, timeout = 11_000) { billingService.runningBillingForProvider() }
+    }
+
+    @Test
+    fun `will not run task on random day of the month`() {
+        val scheduler = InvoiceBillingScheduler(mapOf(Currency.EUR to billingService), Clock.fixed(Instant.parse("2020-01-05T00:00:00Z"), ZoneId.of("UTC")))
+        scheduler.task(Currency.EUR, billingService)
+        verify(exactly = 0, timeout = 11_000) { billingService.runningBillingForProvider() }
+    }
+}
